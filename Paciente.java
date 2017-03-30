1ba2567328b9c0678e88c4001fd58c53a8c8cd74
andreamcm <cor16076@uvg.edu.gt>
2017-03-30 11:18:41 -0600

    Actualización clase Paciente.java

--- Paciente.java
+++ Paciente.java
@@ -2,3 +2,40 @@
 Daniel Rodriguez, 15796
 Fecha: 5 de abril de 2017
 Paciente.java*/
+
+public class Paciente extends Comparable<E>{
+  private String nombre;
+  private String sintomas;
+  private String prioridad;
+
+  public Paciente(){
+    nombre = " ";
+    sintomas = " ";
+    prioridad = " ";
+  }
+
+  public void setNombre(String nombre){
+    this.nombre = nombre;
+  }
+
+  public String getNombre(){
+    return nombre;
+  }
+
+  public void setSintomas(String sintomas){
+    this.sintomas = sintomas;
+  }
+
+  public String getSintomas(){
+    return sintomas;
+  }
+
+  public void setPrioridad(String prioridad){
+    this.prioridad = prioridad;
+  }
+
+  public String getPrioridad(){
+    return prioridad;
+  }
+
+}
