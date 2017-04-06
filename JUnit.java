679863de5fbd482c665550199155a4452333cb70
andreamcm <cor16076@uvg.edu.gt>
2017-04-05 22:25:55 -0600

    JUnit

--- JUnit.java
+++ JUnit.java
@@ -2,3 +2,37 @@
 Daniel Rodriguez, 15796
 Fecha: 5 de abril de 2017
 JUnit.java*/
+
+
+package JUnit;
+import org.junit.*;
+import java.util.Vector;
+
+/**
+ *
+ * @author cesaraugustocordonbalcarcel
+ */
+public class JUnit {
+    
+    @Test
+    public void agregar(){
+        System.out.println("Agregar: \n");
+        String valor = "A";
+        VectorHeap<E> instance = new VectorHeap<E>();
+        instance.add(valor);
+    }
+    
+    @Test
+    public void quitar(){
+        System.out.println("Quitar: \n");
+        String valor2 = "C";
+        String valor3 = "B";
+        VectorHeap<E> instance = new VectorHeap<E>();
+        instance.add(valor2);
+        instance.add(valor3);
+        String resultado = instance.remove("B");
+    }
+    
+    
+}
+
